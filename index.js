068ad041434f7db43b2e96fd9e7b62869ad88011
Nina Nielsen <nina2002katrina@gmail.com>
2023-07-26 13:58:53 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 console.log("hello world");
+
+function sumNum(firstNum, secondNum) {
+   return firstNum + secondNum;
+}
+
+var sumOfNum = sumNum(5,10);
+console.log("Sum of Numbers is: " + sumOfNum);
 
